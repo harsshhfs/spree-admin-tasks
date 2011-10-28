5ec3d0dca7a5d5ac4d105d0d4f4aa5c2f4f5cbd0
Torsten Rüger <torsten@lightning.nu>
2011-10-28 15:41:21 +0300

    cleanup old extension style

--- lib/spree_admin_tasks_hooks.rb
+++ lib/spree_admin_tasks_hooks.rb
@@ -1,16 +1,16 @@
 Deface::Override.new(:virtual_path => "admin/products/_form",
-                     :name => "converted_admin_product_form_right_442423656",
+                     :name => "Adds task link  to product page",
                      :insert_before => "[data-hook='admin_product_form_right'], #admin_product_form_right[data-hook]",
                      :partial => "admin/admin_tasks/product_task_link",
                      :disabled => false)
 
 Deface::Override.new(:virtual_path => "admin/orders/show",
-                     :name => "converted_admin_order_show_addresses_197481537",
+                     :name => "Adds task link to order page",
                      :insert_before => "[data-hook='admin_order_show_addresses'], #admin_order_show_addresses[data-hook]",
                      :partial => "admin/admin_tasks/order_task_link",
                      :disabled => false)
 Deface::Override.new(:virtual_path => "layouts/admin",
-                     :name => "converted_admin_tabs_943317134",
+                     :name => "Adds admin tasks",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
                      :text => " <%= tab :admin_tasks %>",
                      :disabled => false)
